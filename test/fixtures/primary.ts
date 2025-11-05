fb770bce87ec5cd644aa3a19808360ba4bfc5341
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2025-11-05 19:01:12 +0100

    fix test

--- test/fixtures/primary.ts
+++ test/fixtures/primary.ts
@@ -4,6 +4,7 @@ import * as pg from "pg";
 import { setupPrimary } from "../../lib/";
 import { createAdapter } from "@socket.io/cluster-adapter";
 import { Server } from "socket.io";
+import { sleep } from "../util.ts";
 
 const WORKERS_COUNT = 3;
 const EXPECTED_PEERS_COUNT = 3 * WORKERS_COUNT - 1;
@@ -39,14 +40,26 @@ if (cluster.isPrimary) {
     cb(process.pid);
   });
 
-  setTimeout(() => {
+  function isInitComplete() {
+    return io.of("/").adapter.nodesMap.size === EXPECTED_PEERS_COUNT;
+  }
+
+  async function runTest() {
+    io.of("/").adapter.init();
+
+    while (!isInitComplete()) {
+      await sleep(20);
+    }
+
     io.serverSideEmit("test", (err, res) => {
       assert.equal(err, null);
       assert.equal(res.length, EXPECTED_PEERS_COUNT);
 
       setTimeout(() => {
-        process.exit(0);
+        process.exit(0); // exit after having responded to other nodes
       }, GRACE_PERIOD_IN_MS);
     });
-  }, GRACE_PERIOD_IN_MS);
+  }
+
+  runTest();
 }
